12af602aae9060cdb4a3e3474f0259c47ffe11a6
dimi-haris <hariss@godimi.com>
2025-08-21 17:17:40 +0500

    Header responsiveness

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,16 +1,33 @@
 "use client"
 
+import { useState, useEffect, useCallback } from "react"
 import Image from "next/image"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
+import { Bars3Icon } from "@heroicons/react/24/solid"
 
 export default function Header(): React.ReactElement | null {
 	// Get the current pathname
 	const pathname = usePathname()
 
+	// State for the visibility of the mobile menu
+	const [isDropDownOpen, setIsDropDownOpen] = useState<boolean>(false)
+
+	// Memoized callback for handling the dropdown menu toggle
+	const handleToggleDropDown = useCallback((): void => {
+		// Toggle the dropdown menu visibility
+		setIsDropDownOpen((prev) => !prev)
+	}, [])
+
+	// Effect for closing the dropdown menu when the pathname changes
+	useEffect(() => {
+		// Close the dropdown menu
+		setIsDropDownOpen(false)
+	}, [pathname])
+
 	return (
 		// Header container
-		<div className="h-16 w-[72.5%] rounded-full bg-[#1C1C1C08] backdrop-blur-sm border border-[#E0E0E033] absolute top-10 left-[13.75%] z-50 flex flex-row items-center justify-between px-7">
+		<div className="h-16 w-[95%] sm:w-[72.5%] rounded-full sm:bg-[#1C1C1C08] sm:backdrop-blur-sm border border-[#E0E0E033] absolute top-5 sm:top-10 left-[2.5%] sm:left-[13.75%] z-50 flex flex-row items-center justify-between px-2 sm:px-7">
 			{/* Line stroke */}
 			<Image
 				src={"/images/line-stroke.png"}
@@ -29,7 +46,7 @@ export default function Header(): React.ReactElement | null {
 			/>
 			{/* Navigation links */}
 			<div
-				className={`flex flex-row items-center justify-center gap-5 text-nav-link ${
+				className={`hidden sm:flex flex-row items-center justify-center gap-5 text-nav-link ${
 					pathname === "/" || pathname === "/podcast"
 						? "text-white"
 						: "text-heading"
@@ -43,7 +60,32 @@ export default function Header(): React.ReactElement | null {
 				<Link href={"/news"}>News</Link>
 			</div>
 			{/* Empty div to balance the logo */}
-			<div className="w-[50px]" />
+			<div className="hidden sm:flex w-[50px]" />
+			{/* Hamburger menu button */}
+			<button
+				className="sm:hidden flex items-center justify-center mr-2.5"
+				onClick={handleToggleDropDown}
+			>
+				{/* Hamburger menu icon */}
+				<Bars3Icon className="size-6 text-white" />
+			</button>
+			{/* Mobile menu */}
+			{isDropDownOpen && (
+				<div
+					className={`sm:hidden absolute left-0 top-16 h-fit w-full py-5 rounded-4xl bg-[#1C1C1C08] backdrop-blur-sm border border-[#E0E0E033] flex flex-col items-center gap-2.5 font-semibold text-nav-link ${
+						pathname === "/" || pathname === "/podcast"
+							? "text-white"
+							: "text-heading"
+					}`}
+				>
+					<Link href={"/"}>Home</Link>
+					<Link href={"/about"}>About</Link>
+					<Link href={"/blogs"}>Blogs</Link>
+					<Link href={"/podcast"}>Podcast</Link>
+					<Link href={"/services"}>Services</Link>
+					<Link href={"/news"}>News</Link>
+				</div>
+			)}
 		</div>
 	)
 }
