e02108f3d4754725b0c0c3ccfe68e097e304b8e9
dimi-haris <hariss@godimi.com>
2025-09-19 15:06:07 +0500

    News by ID screen designed and API integrated

--- src/app/news/[id]/page.tsx
+++ src/app/news/[id]/page.tsx
@@ -2,10 +2,9 @@
 
 import { use, useState, useEffect } from "react"
 import axios from "axios"
-import SuggestedBlogsSection from "@/components/suggested-blogs-section/SuggestedBlogsSection"
-import { BlogItem } from "@/utils/types"
+import { NewsItem } from "@/utils/types"
 
-export default function BlogPost({
+export default function NewsPost({
 	params
 }: {
 	params: Promise<{ id: string }>
@@ -13,19 +12,19 @@ export default function BlogPost({
 	// Destructure the id from the params
 	const { id } = use(params)
 
-	// State for the blog data
-	const [blog, setBlog] = useState<BlogItem | null>(null)
+	// State for the news data
+	const [news, setNews] = useState<NewsItem | null>(null)
 
 	useEffect(() => {
 		;(async () => {
 			// Initiate a fetch request to the API endpoint
 			await axios
-				.get(`${process.env.NEXT_PUBLIC_API_BASE_URL}/blogs/${id}`)
+				.get(`${process.env.NEXT_PUBLIC_API_BASE_URL}/news/${id}`)
 				.then((response) => {
 					// Log the response data
 					console.log(response.data)
-					// Update the blog state with the fetched data
-					setBlog(response.data)
+					// Update the news state with the fetched data
+					setNews(response.data)
 				})
 				.catch((error) => {
 					// Log the error
@@ -38,11 +37,9 @@ export default function BlogPost({
 		// Main container
 		<div className="min-h-screen w-screen flex flex-col items-center pt-5 sm:pt-40">
 			{/* Blog HTML fragment */}
-			{blog?.blogHtml && (
-				<div dangerouslySetInnerHTML={{ __html: blog?.blogHtml }} />
+			{news?.newsHtml && (
+				<div dangerouslySetInnerHTML={{ __html: news?.newsHtml }} />
 			)}
-			{/* Suggested blogs section */}
-			<SuggestedBlogsSection />
 		</div>
 	)
 }
